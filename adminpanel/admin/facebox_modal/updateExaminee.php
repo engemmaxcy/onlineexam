489d558a6c062068f709f129e277314a0e12756a
Emma <ogangiemmanuel2020@gmail.com>
2025-09-29 23:43:35 +0300

    OK

--- adminpanel/admin/facebox_modal/updateExaminee.php
+++ adminpanel/admin/facebox_modal/updateExaminee.php
@@ -1,83 +1,93 @@
+<?php
+include("../../../conn.php");
+$id = $_GET['id'];
 
-<?php 
-  include("../../../conn.php");
-  $id = $_GET['id'];
- 
-  $selExmne = $conn->query("SELECT * FROM examinee_tbl WHERE exmne_id='$id' ")->fetch(PDO::FETCH_ASSOC);
+$selExmne = $conn->query("SELECT * FROM examinee_tbl WHERE exmne_id='$id' ")->fetch(PDO::FETCH_ASSOC);
+?>
 
- ?>
+<div class="container mt-4">
+   <div class="card shadow-lg border-0 rounded-lg">
+      <div class="card-header bg-primary text-white">
+         <h5 class="mb-0">
+            <i class="edit large icon"></i> Update
+            <b>( <?php echo strtoupper($selExmne['exmne_fullname']); ?> )</b>
+         </h5>
+      </div>
+      <div class="card-body">
+         <form method="post" id="updateExamineeFrm">
+            <input type="hidden" name="exmne_id" value="<?php echo $id; ?>">
 
-<fieldset style="width:543px;" >
-	<legend><i class="facebox-header"><i class="edit large icon"></i>&nbsp;Update <b>( <?php echo strtoupper($selExmne['exmne_fullname']); ?> )</b></i></legend>
-  <div class="col-md-12 mt-4">
-<form method="post" id="updateExamineeFrm">
-     <div class="form-group">
-        <legend>Fullname</legend>
-        <input type="hidden" name="exmne_id" value="<?php echo $id; ?>">
-        <input type="" name="exFullname" class="form-control" required="" value="<?php echo $selExmne['exmne_fullname']; ?>" >
-     </div>
+            <div class="mb-3">
+               <label class="form-label">Fullname</label>
+               <input type="text" name="exFullname" class="form-control" required
+                  value="<?php echo $selExmne['exmne_fullname']; ?>">
+            </div>
 
-     <div class="form-group">
-        <legend>Gender</legend>
-        <select class="form-control" name="exGender">
-          <option value="<?php echo $selExmne['exmne_gender']; ?>"><?php echo $selExmne['exmne_gender']; ?></option>
-          <option value="male">Male</option>
-          <option value="female">Female</option>
-        </select>
-     </div>
+            <div class="mb-3">
+               <label class="form-label">Gender</label>
+               <select class="form-select" name="exGender">
+                  <option value="<?php echo $selExmne['exmne_gender']; ?>">
+                     <?php echo $selExmne['exmne_gender']; ?>
+                  </option>
+                  <option value="male">Male</option>
+                  <option value="female">Female</option>
+               </select>
+            </div>
 
-     <div class="form-group">
-        <legend>Birthdate</legend>
-        <input type="date" name="exBdate" class="form-control" required="" value="<?php echo date('Y-m-d',strtotime($selExmne["exmne_birthdate"])) ?>"/>
-     </div>
+            <div class="mb-3">
+               <label class="form-label">Contact</label>
+               <input type="text" name="exContact" class="form-control" required
+                  value="<?php echo $selExmne['exmne_contact']; ?>" />
+            </div>
 
-     <div class="form-group">
-        <legend>Course</legend>
-        <?php 
-            $exmneCourse = $selExmne['exmne_course'];
-            $selCourse = $conn->query("SELECT * FROM course_tbl WHERE cou_id='$exmneCourse' ")->fetch(PDO::FETCH_ASSOC);
-         ?>
-         <select class="form-control" name="exCourse">
-           <option value="<?php echo $exmneCourse; ?>"><?php echo $selCourse['cou_name']; ?></option>
-           <?php 
-             $selCourse = $conn->query("SELECT * FROM course_tbl WHERE cou_id!='$exmneCourse' ");
-             while ($selCourseRow = $selCourse->fetch(PDO::FETCH_ASSOC)) { ?>
-              <option value="<?php echo $selCourseRow['cou_id']; ?>"><?php echo $selCourseRow['cou_name']; ?></option>
-            <?php  }
-            ?>
-         </select>
-     </div>
+            <div class="mb-3">
+               <label class="form-label">Course</label>
+               <?php
+               $exmneCourse = $selExmne['exmne_course'];
+               $selCourse = $conn->query("SELECT * FROM course_tbl WHERE cou_id='$exmneCourse' ")->fetch(PDO::FETCH_ASSOC);
+               ?>
+               <select class="form-select" name="exCourse">
+                  <option value="<?php echo $exmneCourse; ?>"><?php echo $selCourse['cou_name']; ?></option>
+                  <?php
+                  $selCourse = $conn->query("SELECT * FROM course_tbl WHERE cou_id!='$exmneCourse' ");
+                  while ($selCourseRow = $selCourse->fetch(PDO::FETCH_ASSOC)) { ?>
+                     <option value="<?php echo $selCourseRow['cou_id']; ?>"><?php echo $selCourseRow['cou_name']; ?></option>
+                  <?php  }
+                  ?>
+               </select>
+            </div>
 
-     <div class="form-group">
-        <legend>Year level</legend>
-        <input type="" name="exYrlvl" class="form-control" required="" value="<?php echo $selExmne['exmne_year_level']; ?>" >
-     </div>
+            <div class="mb-3">
+               <label class="form-label">Year level</label>
+               <input type="text" name="exYrlvl" class="form-control" required
+                  value="<?php echo $selExmne['exmne_year_level']; ?>">
+            </div>
 
-     <div class="form-group">
-        <legend>Email</legend>
-        <input type="" name="exEmail" class="form-control" required="" value="<?php echo $selExmne['exmne_email']; ?>" >
-     </div>
+            <div class="mb-3">
+               <label class="form-label">Email</label>
+               <input type="email" name="exEmail" class="form-control" required
+                  value="<?php echo $selExmne['exmne_email']; ?>">
+            </div>
 
-     <div class="form-group">
-        <legend>Password</legend>
-        <input type="" name="exPass" class="form-control" required="" value="<?php echo $selExmne['exmne_password']; ?>" >
-     </div>
+            <div class="mb-3">
+               <label class="form-label">Password</label>
+               <input type="text" name="exPass" class="form-control" required
+                  value="<?php echo $selExmne['exmne_password']; ?>">
+            </div>
 
-     <div class="form-group">
-        <legend>Status</legend>
-        <input type="hidden" name="course_id" value="<?php echo $id; ?>">
-        <input type="" name="newCourseName" class="form-control" required="" value="<?php echo $selExmne['exmne_status']; ?>" >
-     </div>
-  <div class="form-group" align="right">
-    <button type="submit" class="btn btn-sm btn-primary">Update Now</button>
-  </div>
-</form>
-  </div>
-</fieldset>
+            <div class="mb-3">
+               <label class="form-label">Status</label>
+               <input type="hidden" name="course_id" value="<?php echo $id; ?>">
+               <input type="text" name="newCourseName" class="form-control" required
+                  value="<?php echo $selExmne['exmne_status']; ?>">
+            </div>
 
-
-
-
-
-
-
+            <div class="d-flex justify-content-end">
+               <button type="submit" class="btn btn-primary px-4">
+                  Update Now
+               </button>
+            </div>
+         </form>
+      </div>
+   </div>
+</div>
--- adminpanel/admin/pages/manage-examinee.php
+++ adminpanel/admin/pages/manage-examinee.php
@@ -1,76 +1,113 @@
-<link rel="stylesheet" type="text/css" href="css/mycss.css">
+<head>
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- DataTables CSS -->
+    <link href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css" rel="stylesheet">
+
+    <!-- DataTables Buttons CSS -->
+    <link href="https://cdn.datatables.net/buttons/2.4.1/css/buttons.bootstrap5.min.css" rel="stylesheet">
+
+    <!-- Custom CSS -->
+    <link rel="stylesheet" type="text/css" href="css/mycss.css">
+</head>
+<body>
 <div class="app-main__outer">
-        <div class="app-main__inner">
-            <div class="app-page-title">
-                <div class="page-title-wrapper">
-                    <div class="page-title-heading">
-                        <div>MANAGE EXAMINEE</div>
-                    </div>
+    <div class="app-main__inner">
+        <div class="app-page-title">
+            <div class="page-title-wrapper">
+                <div class="page-title-heading">
+                    <div>MANAGE EXAMINEE</div>
                 </div>
-            </div>        
-            
-            <div class="col-md-12">
-                <div class="main-card mb-3 card">
-                    <div class="card-header">Examinee List
-                    </div>
-                    <div class="table-responsive">
-                        <table class="align-middle mb-0 table table-borderless table-striped table-hover" id="tableList">
-                            <thead>
+            </div>
+        </div>
+
+        <div class="col-md-12">
+            <div class="main-card mb-3 card">
+                <div class="card-header">Examinee List</div>
+                <div class="table-responsive">
+                    <table class="align-middle mb-0 table table-borderless table-striped table-hover" id="tableList">
+                        <thead>
                             <tr>
                                 <th>Fullname</th>
                                 <th>Gender</th>
-                                <th>Birthdate</th>
+                                <th>Contact</th>
                                 <th>Course</th>
                                 <th>Year level</th>
                                 <th>Email</th>
                                 <th>Password</th>
-                                <th>status</th>
+                                <th>Status</th>
                                 <th></th>
                             </tr>
-                            </thead>
-                            <tbody>
-                              <?php 
-                                $selExmne = $conn->query("SELECT * FROM examinee_tbl ORDER BY exmne_id DESC ");
-                                if($selExmne->rowCount() > 0)
-                                {
-                                    while ($selExmneRow = $selExmne->fetch(PDO::FETCH_ASSOC)) { ?>
-                                        <tr>
-                                           <td><?php echo $selExmneRow['exmne_fullname']; ?></td>
-                                           <td><?php echo $selExmneRow['exmne_gender']; ?></td>
-                                           <td><?php echo $selExmneRow['exmne_birthdate']; ?></td>
-                                           <td>
-                                            <?php 
-                                                 $exmneCourse = $selExmneRow['exmne_course'];
-                                                 $selCourse = $conn->query("SELECT * FROM course_tbl WHERE cou_id='$exmneCourse' ")->fetch(PDO::FETCH_ASSOC);
-                                                 echo $selCourse['cou_name'];
-                                             ?>
-                                            </td>
-                                           <td><?php echo $selExmneRow['exmne_year_level']; ?></td>
-                                           <td><?php echo $selExmneRow['exmne_email']; ?></td>
-                                           <td><?php echo $selExmneRow['exmne_password']; ?></td>
-                                           <td><?php echo $selExmneRow['exmne_status']; ?></td>
-                                           <td>
-                                               <a rel="facebox" href="facebox_modal/updateExaminee.php?id=<?php echo $selExmneRow['exmne_id']; ?>" class="btn btn-sm btn-primary">Update</a>
+                        </thead>
+                        <tbody>
+                            <?php
+                            $selExmne = $conn->query("SELECT exmne_id, exmne_fullname, exmne_gender, exmne_contact, exmne_course, exmne_year_level, exmne_email, exmne_password, exmne_status FROM examinee_tbl ORDER BY exmne_id DESC");
 
-                                           </td>
-                                        </tr>
-                                    <?php }
-                                }
-                                else
-                                { ?>
+                            if ($selExmne->rowCount() > 0) {
+                                while ($selExmneRow = $selExmne->fetch(PDO::FETCH_ASSOC)) { ?>
                                     <tr>
-                                      <td colspan="2">
-                                        <h3 class="p-3">No Course Found</h3>
-                                      </td>
+                                        <td><?= $selExmneRow['exmne_fullname']; ?></td>
+                                        <td><?= $selExmneRow['exmne_gender']; ?></td>
+                                        <td><?= $selExmneRow['exmne_contact']; ?></td>
+                                        <td>
+                                            <?php
+                                            $exmneCourse = $selExmneRow['exmne_course'];
+                                            $selCourse = $conn->query("SELECT cou_name FROM course_tbl WHERE cou_id='$exmneCourse' ")->fetch(PDO::FETCH_ASSOC);
+                                            echo $selCourse['cou_name'];
+                                            ?>
+                                        </td>
+                                        <td><?= $selExmneRow['exmne_year_level']; ?></td>
+                                        <td><?= $selExmneRow['exmne_email']; ?></td>
+                                        <td><?= $selExmneRow['exmne_password']; ?></td>
+                                        <td><?= $selExmneRow['exmne_status']; ?></td>
+                                        <td>
+                                            <a rel="facebox" href="facebox_modal/updateExaminee.php?id=<?= $selExmneRow['exmne_id']; ?>" class="btn btn-sm btn-primary">Update</a>
+                                        </td>
                                     </tr>
                                 <?php }
-                               ?>
-                            </tbody>
-                        </table>
-                    </div>
+                            } else { ?>
+                                <tr>
+                                    <td colspan="9">
+                                        <h3 class="p-3">No Course Found</h3>
+                                    </td>
+                                </tr>
+                            <?php } ?>
+                        </tbody>
+                    </table>
                 </div>
             </div>
-      
-        
+        </div>
+    </div>
 </div>
-         
+
+<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.13.6/js/dataTables.bootstrap5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.bootstrap5.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.10.1/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.print.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/2.4.1/js/buttons.colVis.min.js"></script>
+
+<script>
+$(document).ready(function() {
+    $('#tableList').DataTable({
+        dom: 'Bfrtip',
+        buttons: [
+            { extend: 'copy', className: 'btn btn-sm btn-outline-primary' },
+            { extend: 'excel', className: 'btn btn-sm btn-outline-success' },
+            { extend: 'pdf', className: 'btn btn-sm btn-outline-danger' },
+            { extend: 'print', className: 'btn btn-sm btn-outline-dark' },
+            { extend: 'colvis', className: 'btn btn-sm btn-outline-secondary' }
+        ],
+        pageLength: 10,
+        responsive: true
+    });
+});
+</script>
+</body>
